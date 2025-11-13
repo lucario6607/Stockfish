55c0b0529db1c99ae18f951842f5260cf1043aeb
lucario6607 <bowlingkings@icloud.com>
2025-11-12 23:13:09 -0600

    take2

--- src/search.cpp
+++ src/search.cpp
@@ -1109,7 +1109,7 @@ moves_loop:  // When in check, search starts here
             && ttData.depth >= depth - 3)
         {
             Value singularBeta  = ttData.value - (56 + 81 * (ss->ttPv && !PvNode) + 35 * !ttCapture) * depth / 60;
-            Depth singularDepth = (newDepth / 2) + !ttCapture;
+            Depth singularDepth = newDepth / 2;
 
             ss->excludedMove = move;
             value = search<NonPV>(pos, ss, singularBeta - 1, singularBeta, singularDepth, cutNode);
